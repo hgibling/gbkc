b8c87a9b8ef82bc6e1f1d2cdb05133482536dc21
Heather Gibling <heathergibling@gmail.com>
2019-04-23 13:50:28 -0400

    add placeholder for distance function

---
+++ src/distance.cpp
@@ -0,0 +1,27 @@
+// distance - Score reads based on comparison of k-mer pair distances to those for known alleles
+
+#include <iostream>
+#include <string>
+
+#include "distance.h"
+
+
+#define SUBPROGRAM "distance"
+
+// static const char *DISTANCE_USAGE_MESSAGE =
+// "Usage: " PACKAGE_NAME " " SUBPROGRAM " [OPTION] ... READSFILE\n"
+// "Correct sequencing errors in all the reads in READSFILE\n"
+// "\n"
+// "      --help                           display this help and exit\n";
+
+// static const char* PROGRAM_IDENT =
+// PACKAGE_NAME "::" SUBPROGRAM;
+
+
+
+int distanceMain(int argc, char** argv) {
+
+	printf("Distance function under construction\n");
+
+	return 0;
+}
