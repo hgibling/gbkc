e887391a44031a9be856335d85c9074291a805ed
Heather Gibling <heathergibling@gmail.com>
2019-07-25 16:00:04 -0400

    fix typo

--- src/check-profiles.h
+++ src/check-profiles.h
@@ -17,7 +17,7 @@
 typedef std::map<std::string, size_t> kmer_count_map;
 
 // Define k-mer count comparison structure
-typedef std::map<std::string, int> kmer_comparsion_map;
+typedef std::map<std::string, int> kmer_comparison_map;
 
 // Define sequence record structre
 struct sequence_record
